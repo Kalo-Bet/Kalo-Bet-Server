c2cd1eca8fee0fde797bb181d2398e59d9d90ba0
emmanuel-uzoezie <uzoezieemmanuel@gmail.com>
2024-08-11 05:53:42 +0100

    trying to fix cors error

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    
-    // Enable CORS
     app.enableCors({
-      origin: ['http://localhost:5173', 'https://www.kalo.bet', 'https://kalo.bet'],
+      origin: true,
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
       credentials: true,
