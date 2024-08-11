064a2cceccb85a93c6bca961c6a1a446a859a650
emmanuel-uzoezie <uzoezieemmanuel@gmail.com>
2024-08-11 05:47:13 +0100

    added cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { BetModule } from './bet/bet.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import * as cors from 'cors';
 
 @Module({
   imports: [
@@ -14,4 +15,15 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env',
     }),]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(cors({
+        origin: ['http://localhost:5173', 'https://www.kalo.bet', 'https://kalo.bet'],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+        credentials: true,
+      }))
+      .forRoutes('*');
+  }
+}
